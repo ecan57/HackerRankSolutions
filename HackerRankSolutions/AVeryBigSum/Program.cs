a388b9c2edd2a453db9df49ebc70972eb0521346
Emine CAN <eminecan570@gmail.com>
2024-07-01 11:58:25 +0300

    A Very Big Sum

---
+++ HackerRankSolutions/AVeryBigSum/Program.cs
@@ -0,0 +1,35 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+        foreach (long num in ar)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
